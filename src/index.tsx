429158741f6c88e89eb86f15d460a6c0029dac28
seriksk1 <seriksk1@ukr.net>
2022-09-13 22:03:07 +0300

    Feature: Initialize the redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from "react"
+import { Provider } from "react-redux"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import { AppRoutes } from "./routes"
+import { store } from "./store"
 import "./index.pcss"
 
 const container = document.getElementById("root")
 const root = createRoot(container!)
 root.render(
-  <BrowserRouter>
-    <AppRoutes />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  </Provider>
 )
